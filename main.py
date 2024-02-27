7250dbd65821b01d0aec4f5dd4df79fe67ad6c04
Dominik Filliger <filligerdominik@gmail.com>
2024-02-27 12:07:45 +0100

    we done

---
+++ main.py
@@ -0,0 +1,25 @@
+from transformers import AutoModel, AutoTokenizer
+import chromadb
+from chromadb.utils import embedding_functions
+default_ef = embedding_functions.DefaultEmbeddingFunction()
+
+
+if __name__ == "__main__":
+    text = "This is a test sentence."
+
+    embedding = default_ef([text])
+
+    client = chromadb.Client()
+    collection = client.create_collection("sample_collection")
+
+    print(embedding)
+
+    print(type(embedding))
+    # Add docs to the collection. Can also update and delete. Row-based API coming soon!
+    collection.add(
+        documents=[text],
+        metadatas=[{"source": "my_source"}],
+        ids=["test1"],
+        embeddings=embedding,
+    )
+
