974949d5b8ff27b3706f03d4307abcef1a538635
Nils <nils.fahrni@students.fhnw.ch>
2024-03-12 11:12:38 +0100

    added langfuse dataset functions

--- src/langfuse.py
+++ src/langfuse.py
@@ -8,10 +8,16 @@ from langfuse import Langfuse
 load_dotenv()
 langfuse = Langfuse()
 
+class DatasetItem:
+    def __init__(self, input_str, output_str):
+        self.input_str = input_str
+        self.output_str = output_str
+
+    def __repr__(self):
+        return f"IOData(input={self.input!r}, output={self.output!r})"
 
 def generate_session_id():
     return ''.join(random.choices(string.ascii_letters + string.digits, k=30))
-
 
 def get_langfuse_handler():
     # TODO: move callback handler to context specific call to track on a per chat basis
@@ -31,6 +37,20 @@ def get_langfuse_handler():
     assert handler.auth_check()
 
     return handler
+
+def create_new_dataset(name: str):
+    langfuse.create_dataset(name=name)
+
+def add_item_to_dataset(dataset_name: str, item: DatasetItem):
+    langfuse.create_dataset_item(
+        dataset_name=dataset_name,
+        input={
+            "text": item.input_str
+        },
+        expected_output={
+            "text": item.output_str
+        }
+    )
 
 # TODO: implemnent possibility to score traces
 # https://langfuse.com/docs/sdk/python#scores
