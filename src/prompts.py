c64033b443b1683a34e5171c409dee7a07ecb167
Dominik Filliger <filligerdominik@gmail.com>
2024-03-19 13:17:36 +0100

    Changed Generator to be dynamic for analysis

--- src/prompts.py
+++ src/prompts.py
@@ -2,17 +2,19 @@ from langchain_core.prompts import ChatPromptTemplate
 
 from src.langfuse import langfuse
 
-PROMPTS = {
-    "base_template":
-        ("""Answer the question to your best knowledge when looking at the following context:
+
+
+class Prompt:
+
+    DEFAULT_PROMPT = {
+        "base_template":
+            ("""Answer the question to your best knowledge when looking at the following context:
         {context}
 
         Question: {question}
         """, 1)
-}
+    }
 
-
-class Prompt:
     def __init__(self,
                  key: str,
                  template: ChatPromptTemplate,
@@ -33,6 +35,6 @@ class Prompt:
             template = ChatPromptTemplate.from_template(langfuse_prompt.get_langchain_prompt())
             return Prompt(key, template, langfuse_prompt.version)
 
-        prompt = PROMPTS[key]
+        prompt = Prompt.DEFAULT_PROMPT[key]
         return Prompt(key, ChatPromptTemplate.from_template(prompt[0]), prompt[1])
 
