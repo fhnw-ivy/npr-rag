8ace0d1356471142390100c1d23caccdc8ffa947
Noah <thenoahleuenberger@gmail.com>
2024-04-02 16:43:33 +0200

    Added max retries option

--- src/generation.py
+++ src/generation.py
@@ -7,6 +7,8 @@ from langchain_openai import ChatOpenAI, AzureChatOpenAI
 
 load_dotenv()
 
+MAX_RETRIES = 100
+
 
 class LLMModel(Enum):
     GPT_3_AZURE = "gpt-3-azure"
@@ -16,13 +18,15 @@ class LLMModel(Enum):
 def get_llm_model(model: LLMModel = LLMModel.GPT_3_AZURE):
     if model == LLMModel.GPT_3_5_TURBO:
         openai.api_key = os.getenv("OPENAI_API_KEY")
-        return ChatOpenAI(model_name="gpt-3.5-turbo")
+        return ChatOpenAI(model_name="gpt-3.5-turbo",
+                          max_retries=MAX_RETRIES)
 
     if model == LLMModel.GPT_3_AZURE:
         openai.api_key = os.getenv("AZURE_OPENAI_API_KEY")
         return AzureChatOpenAI(
             openai_api_version="2023-05-15",
-            azure_deployment=os.getenv("AZURE_OPENAI_DEPLOYMENT")
+            azure_deployment=os.getenv("AZURE_OPENAI_DEPLOYMENT"),
+            max_retries=MAX_RETRIES
         )
 
     raise ValueError(f"Model {model} not supported.")
